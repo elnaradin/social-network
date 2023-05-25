8b6de71b3da5aee961f657529d2d3d89492fc845
dmitrii_kapriz <dkapriz@mail.ru>
2023-05-25 19:35:05 +0300

    added auth

--- microservice-friends/main/src/main/java/ru/itgroup/intouch/aspect/SecurityAspect.java
+++ microservice-friends/main/src/main/java/ru/itgroup/intouch/aspect/SecurityAspect.java
@@ -1,24 +1,26 @@
 package ru.itgroup.intouch.aspect;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import model.account.Account;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.itgroup.intouch.repository.AccountRepository;
+import ru.itgroup.intouch.client.AccountServiceClient;
+import ru.itgroup.intouch.dto.AccountDto;
+import ru.itgroup.intouch.mapper.FriendMapper;
+
+import javax.security.sasl.AuthenticationException;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityAspect {
-
-    private final AccountRepository accountRepository;
-
-    @Autowired
-    public SecurityAspect(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
+    private final AccountServiceClient accountServiceClient;
+    private final FriendMapper friendMapper;
 
     @Pointcut("@annotation(ru.itgroup.intouch.aspect.CheckAndGetAuthUser) && (args(..,account) || args(account))")
     public void getSecurityUser(Account account) {
@@ -27,15 +29,12 @@ public class SecurityAspect {
     @Around(value = "getSecurityUser(account)", argNames = "proceedingJoinPoint,account")
     public Object execAdviceForGetSecurityUser(ProceedingJoinPoint proceedingJoinPoint, Account account)
             throws Throwable {
-
-        //TODO добавить проверку и получение пользователя из account сервиса
-        //TODO если пользователь не авторизован выбросить исключение
-        //TODO объект currentAccount должен передавать текущего пользователя
-        Account currentAccount = accountRepository.getReferenceById(1L);
-
-
+        AccountDto accountDto = accountServiceClient.myAccount();
+        if (accountDto == null) {
+            throw new AuthenticationException();
+        }
+        Account currentAccount = friendMapper.accountDtoToAccount(accountDto);
         proceedingJoinPoint.getArgs()[proceedingJoinPoint.getArgs().length - 1] = currentAccount;
         return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
     }
-
 }
