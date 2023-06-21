c0b8cb4fb74726679d2ad438072751a2ffd4cd45
SkellFun <skellfun@gmail.com>
2023-06-21 19:35:22 +0400

    some fixes

--- geo-service/main/src/main/java/ru/itgroup/intouch/controller/GeoController.java
+++ geo-service/main/src/main/java/ru/itgroup/intouch/controller/GeoController.java
@@ -1,8 +1,10 @@
 package ru.itgroup.intouch.controller;
 
 
+import model.City;
 import model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itgroup.intouch.service.GeoService;
@@ -16,19 +18,19 @@ public class GeoController {
     GeoService geoService;
 
     @PutMapping("/load")
-    public ResponseEntity loadGeo(){
+    public ResponseEntity<?> loadGeo(){
         geoService.loadGeo();
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/country")
-    public ResponseEntity getCountries(){
+    @GetMapping(value = "/country", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Country>> getCountries(){
         List<Country> countries = geoService.getCountries();
         return ResponseEntity.ok(countries);
     }
 
-    @GetMapping("/country/{countryId}/cities")
-    public ResponseEntity getCities(@PathVariable Long countryId){
+    @GetMapping(value = "/country/{countryId}/cities", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<City>> getCities(@PathVariable Long countryId){
         return ResponseEntity.ok(geoService.getCities(countryId));
     }
 }
