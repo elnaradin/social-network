06e467be671aad64ae63f01da5f60cc0aa457e98
Александр Акулов <skellfun@gmail.com>
2023-06-24 17:32:33 +0300

    Aggregator geo

--- aggregator/src/main/java/ru/itgroup/intouch/aggregator/controller/GeoController.java
+++ aggregator/src/main/java/ru/itgroup/intouch/aggregator/controller/GeoController.java
@@ -1,6 +1,7 @@
 package ru.itgroup.intouch.aggregator.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itgroup.intouch.client.GeoServiceClient;
@@ -17,13 +18,15 @@ public class GeoController {
         return client.feignLoadGeo();
     }
 
-    @GetMapping("/country")
-    public ResponseEntity getCountries(){
-        return client.feignGetCountries();
+    @GetMapping(value = "/country", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getCountries(){
+        String s = client.feignGetCountries();
+        System.out.println(" ");
+        return s;
     }
 
-    @GetMapping("/country/{countryId}/cities")
-    public ResponseEntity getCities(@PathVariable Long countryId){
+    @GetMapping(value = "/country/{countryId}/cities", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getCities(@PathVariable Long countryId){
         return client.feignGetCities(countryId);
     }
 }
