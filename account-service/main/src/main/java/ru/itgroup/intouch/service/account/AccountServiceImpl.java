78572524a4ee14bb859f0054886dbd8f40ba77f1
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-06-26 20:50:50 +0500

    refactor

---
+++ account-service/main/src/main/java/ru/itgroup/intouch/service/account/AccountServiceImpl.java
@@ -0,0 +1,93 @@
+package ru.itgroup.intouch.service.account;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import model.account.Account;
+import model.account.User;
+import org.springframework.stereotype.Service;
+import ru.itgroup.intouch.dto.AccountDto;
+import ru.itgroup.intouch.dto.UserDto;
+import ru.itgroup.intouch.exceptions.NoUserRegisteredException;
+import ru.itgroup.intouch.mapper.UserMapper;
+import ru.itgroup.intouch.repository.AccountRepository;
+import ru.itgroup.intouch.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AccountServiceImpl implements AccountService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final AccountRepository accountRepository;
+
+    public AccountDto getAccountInfo(String email) {
+        Optional<Account> account = accountRepository
+                .findFirstByEmailEqualsAndIsDeletedEquals(email, false);
+        if (account.isEmpty()) {
+            throw new NoUserRegisteredException("Аккаунт с адресом \"" +
+                    email + "\" не найден");
+        }
+        return userMapper.accountEntityToAccountDto(account.get());
+    }
+
+    public UserDto getUserInfo(String email) {
+        if (!userRepository.existsByEmail(email)) {
+            throw new NoUserRegisteredException("Неверный логин или пароль");
+        }
+        Optional<User> user = userRepository
+                .findFirstByEmailEqualsAndIsDeletedEquals(email, false);
+        if (user.isPresent()) {
+            return userMapper.userEntity2UserDto(user.get());
+        }
+        throw new NoUserRegisteredException("Пользователь с адресом \"" +
+                email + "\" удалил свой аккаунт");
+    }
+
+    @Transactional
+    public void updateAccountData(AccountDto accountDto) {
+        Optional<Account> account = accountRepository
+                .findFirstByEmailEqualsAndIsDeletedEquals(accountDto.getEmail(), false);
+        log.info("change account with email \"" + accountDto.getEmail() + "\"");
+        if (account.isEmpty()) {
+            throw new NoUserRegisteredException("Невозможно изменить данные аккаунта. E-mail \"" +
+                    accountDto.getEmail() + "\" не найден");
+        }
+        Account accountEntity = account.get();
+        userMapper.updateAccountFromDto(accountDto, accountEntity);
+        accountEntity.setUpdateOn(LocalDateTime.now());
+        accountRepository.save(accountEntity);
+    }
+
+    @Transactional
+    public void setAccountDeleted(String email) {
+        Optional<User> firstByEmail = userRepository
+                .findFirstByEmailEqualsAndIsDeletedEquals(email, false);
+        if (firstByEmail.isEmpty()) {
+            throw new NoUserRegisteredException("Невозможно удалить аккаунт. " +
+                    "E-mail \"" + email + "\" не найден.");
+        }
+        User user = firstByEmail.get();
+        user.setDeleted(true);
+        userRepository.save(firstByEmail.get());
+    }
+
+    public List<AccountDto> getListOfUsers(List<Long> userIds) {
+        List<Account> accounts = accountRepository.findByIdIn(userIds);
+        return userMapper.accountsToDtos(accounts);
+    }
+
+
+    public AccountDto getAccountInfoById(Long id) {
+        Optional<Account> account = accountRepository.findById(id);
+
+        if(account.isEmpty()){
+            throw new NoUserRegisteredException("Account not found");
+        }
+        return userMapper.accountEntityToAccountDto(account.get());
+    }
+}
--- account-service/main/src/main/java/ru/itgroup/intouch/service/account/CredentialsRenewalService.java
+++ account-service/main/src/main/java/ru/itgroup/intouch/service/account/CredentialsRenewalService.java
@@ -1,91 +1,9 @@
 package ru.itgroup.intouch.service.account;
 
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import model.account.User;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 import ru.itgroup.intouch.config.email.EmailContents;
-import ru.itgroup.intouch.exceptions.NoEmailFoundException;
-import ru.itgroup.intouch.exceptions.NoUserRegisteredException;
-import ru.itgroup.intouch.repository.UserRepository;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class CredentialsRenewalService {
-    @Value("${EMAIL_FROM}")
-    private String from;
-    private final JavaMailSender mailSender;
-    private final UserRepository userRepository;
-    @Value("${user.hash-expiry-min}")
-    private long expiryMinutes;
-    private final SpringTemplateEngine templateEngine;
-
-    public void setNewPassword(String linkId, String password) {
-        Optional<User> firstByEmail = userRepository.findFirstByHash(linkId);
-        if (firstByEmail.isEmpty()) {
-            throw new NoUserRegisteredException("Не удалось изменить пароль. Запрос на изменение пароля не найден");
-        }
-        firstByEmail.get().setPassword(password);
-        userRepository.save(firstByEmail.get());
-    }
-
-    public void sendLetter(String to, EmailContents contents, boolean addLinkId) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(
-                message,
-                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                StandardCharsets.UTF_8.name()
-        );
-        Context context = new Context();
-        Optional<User> optUser = userRepository.findFirstByEmailEqualsAndIsDeletedEquals(to, false);
-        if (optUser.isEmpty()) {
-            throw new NoEmailFoundException("Пользователь с адресом \"" + to + "\" не зарегистрирован");
-        }
-        User user = optUser.get();
-        helper.setFrom(from);
-        helper.setTo(to);
-        helper.setSubject(contents.getSubject().toUpperCase(Locale.ROOT));
-
-        String userHash = "";
-        if (addLinkId) {
-            userHash = UUID.randomUUID().toString();
-            user.setHash(userHash);
-            user.setHashExpiryTime(LocalDateTime.now().plusMinutes(expiryMinutes));
-            userRepository.save(user);
-        }
-
-        context.setVariables(Map.of(
-                "subject", contents.getSubject(),
-                "name", user.getFirstName(),
-                "url", contents.getUrl() + userHash)
-        );
-        String html = templateEngine.process("letter.html", context);
-        helper.setText(html, true);
-        mailSender.send(message);
-    }
-
-    @Scheduled(fixedDelayString = "${delete-hash.delay-ms}")
-    @Transactional
-    public void deleteHashWithDelay() {
-        userRepository.clearHashAfterExpiry(LocalDateTime.now());
-    }
-
-
+public interface CredentialsRenewalService {
+    void setNewPassword(String linkId, String password);
+    void sendLetter(String to, EmailContents contents, boolean addLinkId) throws MessagingException;
 }
